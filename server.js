e490d391af886bd7c9a583ebadb088c1612e498e
Glitch (glitch-hello-node) <none>
2023-09-03 19:06:53 +0000

    💅🐚 Checkpoint ./package.json:2717153/1059 ./public/index.html:2717153/5005 ./server.js:2717153/8917

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ const server = http.createServer(app);
 // Create a WebSocket server by passing the HTTP server instance
 const wss = new WebSocket.Server({ server });
 
+// Store mouse positions
+const mousePositions = [];
+
 // Define a route to serve your black and red page
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
@@ -22,19 +25,55 @@ app.get('/', (req, res) => {
 wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     console.log(`Received message: ${message}`);
-    if (message === 'recordClick') {
-      const currentDate = new Date().toLocaleString();
-      fs.appendFile('clicks.txt', `${currentDate}\n`, (err) => {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log('Click recorded.');
+    if (typeof message === 'string') {
+      if (message === 'recordClick') {
+        const currentDate = new Date().toLocaleString();
+        fs.appendFile('clicks.txt', `${currentDate}\n`, (err) => {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log('Click recorded.');
+          }
+        });
+      } else if (message.startsWith('mousePosition')) {
+        const [, mouseX, mouseY] = message.split(',');
+        const timestamp = Date.now();
+        mousePositions.push({ timestamp, x: mouseX, y: mouseY });
+
+        // Limit the array size to 30 records per second
+        if (mousePositions.length > 30) {
+          mousePositions.shift();
         }
-      });
+
+        // Send mouse position to all clients
+        wss.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(`mousePosition,${mouseX},${mouseY}`);
+          }
+        });
+
+        // Write the mouse positions to a file every second
+        if (mousePositions.length === 30) {
+          const dataToWrite = mousePositions
+            .map(({ timestamp, x, y }) => `${timestamp},${x},${y}`)
+            .join('\n');
+          fs.appendFile('mouse_positions.txt', dataToWrite, (err) => {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log('Mouse positions recorded.');
+            }
+          });
+          mousePositions.length = 0; // Clear the array
+        }
+      }
     }
   });
 });
 
+
+
 const listener = server.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
